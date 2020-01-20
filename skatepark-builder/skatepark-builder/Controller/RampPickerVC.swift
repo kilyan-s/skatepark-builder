5c63624530e47f42f6a9521590092d16be724f09
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-20 12:14:23 -0500

    Add 3D models to popover picker VC.

--- skatepark-builder/skatepark-builder/Controller/RampPickerVC.swift
+++ skatepark-builder/skatepark-builder/Controller/RampPickerVC.swift
@@ -30,6 +30,41 @@ class RampPickerVC: UIViewController {
         view.insertSubview(sceneView, at: 0)
         
         preferredContentSize = size
+        
+        //Create the scene to add all 3D models
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        //Create a custom camera to remove depth, only x,y axis
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        //Create pipe 3D object
+        let pipeScene = SCNScene(named: "art.scnassets/pipe.dae")
+        //Get the pipe object in pipeScene file
+        let pipeNode = pipeScene?.rootNode.childNode(withName: "pipe", recursively: true)
+        //Change the scale and position to fit in popover VC
+        pipeNode?.scale = SCNVector3Make(0.0020, 0.0020, 0.0020)
+        pipeNode?.position = SCNVector3Make(0.1, 0.7, 0)
+        
+        //Create pyramid 3D object
+        let pyramidScene = SCNScene(named: "art.scnassets/pyramid.dae")
+        let pyramidNode = pyramidScene?.rootNode.childNode(withName: "pyramid", recursively: true)
+        pyramidNode?.scale = SCNVector3Make(0.0050, 0.0050, 0.0050)
+        pyramidNode?.position = SCNVector3Make(0.1, -0.2, 0)
+        
+        //Create quarter 3D Object
+        let quarterScene = SCNScene(named: "art.scnassets/quarter.dae")
+        let quarterNode = quarterScene?.rootNode.childNode(withName: "quarter", recursively: true)
+        quarterNode?.scale = SCNVector3Make(0.0045, 0.0045, 0.0045)
+        quarterNode?.position = SCNVector3Make(0.1, -1.5, 0)
+        
+        //Add objects to the main scene
+        scene.rootNode.addChildNode(pipeNode!)
+        scene.rootNode.addChildNode(pyramidNode!)
+        scene.rootNode.addChildNode(quarterNode!)
+        
     }
     
 }
